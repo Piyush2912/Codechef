6432b828371b8595e6e1034f2f561248f7c9ce14
Piyush <piyushkatariya95@gmail.com>
2020-02-11 01:06:20 +0530

    Hard Cash Incomplete

---
+++ src/February_Long/Hard_Cash.java
@@ -0,0 +1,18 @@
+package February_Long;
+
+import java.util.Scanner;
+
+public class Hard_Cash {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+        }
+    }
+}
